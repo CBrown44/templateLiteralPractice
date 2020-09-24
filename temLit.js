4f56365f4dd883b4b023cfdb950be99fa2d8c837
Caleb Brown <cbrown200@west-mec.org>
2020-09-23 17:57:08 -0700

    Fixed mistakes

--- temLit.js
+++ temLit.js
@@ -12,14 +12,14 @@ let lastName = prompt("Please enter last Name");
 lastName;
 
 // Create a variable that will hold a score and a var that will hold a full name 
-let fullName = firstName +" " + lastName;
+let fullName = `${firstName} ${lastName}`;
 
-let score = 100;
+let score = 180;
 
 
 // create a var that adds the full name var and score in a message
-let joined = ${fullName} Has a Score of   ${score};
-console.log(joined);
+let joined = `${fullName} Has a Score of ${score}`;
+console.log(`${joined}`);
 
 // ////////////////////////Pt. 2////////////////////////
 // //comment out the lines above then create two var's that hold numbers
@@ -34,8 +34,8 @@ let add = numOne + numTwo;
 console.log(add); 
 
 // //console log 3 string's using concatination that say "concatinating is easy""when using template literals,""I can even embed numbers and not have to worry about spacing!"
-console.log( concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing!")
+console.log( `concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing!`)
 
 
 // //console log the three strings above and add a third string that includes the add var
-console.log(concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing!" ${add} )*/
+console.log(`concatinating is easy when using template literals, I can even embed numbers and not have to worry about spacing!" ${add}` )*/
